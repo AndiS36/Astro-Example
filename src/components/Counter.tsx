4d4fe06dfa03ebb9520b2eafaab4f8f463554364
Anand Sutar <computerit1995@gmail.com>
2022-11-30 14:40:26 +0530

    Astro react angular added

---
+++ src/components/Counter.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './Counter.css';
+
+export default function Counter() {
+	return (
+		<>
+			<div className="counter-message">{'Hello React'}</div>
+		</>
+	);
+}
---
+++ src/components/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    template: `<div>
+      <h1>Hello Angular Astro</h1>
+    </div>`,
+  })
+  export class HeaderComponent {
+  }
